466acb6f21018101a6653eb8fdee01f81167d69b
honey1b <nh822@naver.com>
2020-08-01 17:09:09 +0900

    test09

---
+++ test09/src/com/servelt/ServeltGet.java
@@ -0,0 +1,29 @@
+package com.servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/seg")
+public class ServeltGet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String connectedIP=(String)getServletContext().getAttribute("connectedIP");
+		String connedtedUser=(String)getServletContext().getAttribute("connectedUser");
+		
+		PrintWriter out=response.getWriter();
+		out.print("<p>connectedIP : "+connectedIP+"</p>");
+		out.print("<p>connedtedUser : "+connedtedUser+"</p>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
