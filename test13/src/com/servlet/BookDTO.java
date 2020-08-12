8a46c5ebf8e731388574c736d3d4d3538bbc7a3a
honey1b <nh822@naver.com>
2020-08-12 21:21:24 +0900

    test13 -> connection pool 이용

---
+++ test13/src/com/servlet/BookDTO.java
@@ -0,0 +1,32 @@
+package com.servlet;
+
+public class BookDTO {
+	int bookId;
+	String bookName;
+	String bookLoc;
+	
+	public BookDTO(int bookId,String bookName,String bookLoc) {
+		this.bookId=bookId;
+		this.bookLoc=bookLoc;
+		this.bookName=bookName;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public String getBookLoc() {
+		return bookLoc;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public void setBookLoc(String bookLoc) {
+		this.bookLoc = bookLoc;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+}
